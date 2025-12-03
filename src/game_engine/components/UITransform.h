16817a29986c01874c5ff9a2baf17e3e6a22eea0
JeanBizeul <jean.bizeul@epitech.eu>
2025-12-03 11:34:49 +0100

    ✨ Add UITransform component

---
+++ src/game_engine/components/UITransform.h
@@ -0,0 +1,23 @@
+namespace ecs {
+
+namespace cpnt {
+
+// 2D Transform component used for UI elements.
+// Stores position, z-layer, size, achor point & rotation.
+struct UITransform {
+    float x;
+    float y;
+    float z;
+
+    float w;
+    float h;
+
+    float achorX;
+    float achorY;
+
+    float rotation;
+};
+
+} // namespace components
+
+} // namespace ecs
