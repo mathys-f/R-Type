da0b68e010e3618d779f62529226cecba09a1afc
JeanBizeul <jean.bizeul@epitech.eu>
2025-12-03 12:43:30 +0100

    ✨ Add Tag component

---
+++ src/game_engine/components/tag.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ecs/tag_registry.h"
+
+namespace engn {
+
+namespace cpnt {
+
+struct Tag {
+    ecs::TagRegistry::TagId id;
+};
+
+} // namespace cpnt
+
+} // namespace engn
