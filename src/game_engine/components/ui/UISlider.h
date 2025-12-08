c3d1f40b1e4a88dcb54192a1869fe39583336ac7
JeanBizeul <jean.bizeul@epitech.eu>
2025-12-08 11:47:07 +0100

    ✨ Add UISlider component

---
+++ src/game_engine/components/ui/UISlider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace engn {
+
+namespace cpnt {
+
+struct UISlider {
+    float value;
+    float min_value;
+    float max_value;
+};
+
+} // namespace cpnt
+
+} // namespace engn
