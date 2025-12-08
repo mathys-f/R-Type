d1ac731112816520117ba436fe6b732721097dfd
JeanBizeul <jean.bizeul@epitech.eu>
2025-12-08 11:45:37 +0100

    ✨ Add UIText component

---
+++ src/game_engine/components/ui/ui_text.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+namespace engn {
+
+namespace cpnt {
+
+struct UIText {
+    std::string content;
+    int font_size;
+};
+
+} // namespace cpnt
+
+} // namespace engn
