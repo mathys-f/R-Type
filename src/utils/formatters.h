0a989b7629b3c2e87cc31df6bf5df1ab0bd80272
JeanBizeul <jean.bizeul@epitech.eu>
2025-12-09 17:11:39 +0100

    ✨ Add a formatter for ecs::Entity

--- src/utils/formatters.h
+++ src/utils/formatters.h
@@ -10,3 +10,10 @@ struct std::formatter<std::filesystem::path> : std::formatter<std::string> {
         return std::formatter<std::string>::format(p.string(), ctx);
     }
 };
+
+template <>
+struct std::formatter<ecs::Entity> : std::formatter<size_t> {
+    auto format(const ecs::Entity &entity, auto &ctx) const {
+        return std::formatter<size_t>::format(static_cast<size_t>(entity), ctx);
+    }
+};
