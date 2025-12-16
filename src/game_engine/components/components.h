718159e1775becf869aec85890643afec5e0b5ff
JeanBizeul <jean.bizeul@epitech.eu>
2025-12-16 14:32:23 +0100

    ✨ Add enum of all snapshotable components

--- src/game_engine/components/components.h
+++ src/game_engine/components/components.h
@@ -17,6 +17,28 @@
 #include "weapon.h"
 #include "sprite.h"
 
+namespace engn::cpnt {
+
+enum ComponentType {
+    Bullet,
+    Controllable,
+    Enemy,
+    Explosion,
+    Health,
+    Hitbox,
+    MovementPattern,
+    Player,
+    Replicated,
+    Stats,
+    Tag,
+    Transform,
+    Velocity,
+    Weapon,
+    Sprite
+};
+
+} // namespace engn::cpnt
+
 // Non headless components
 #include "ui/ui_button.h"
 #include "ui/ui_checkbox.h"
--- src/game_engine/components/particle.h
+++ src/game_engine/components/particle.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "snapshot_meta.h"
-
 namespace engn {
 
 namespace cpnt {
@@ -9,8 +7,6 @@ namespace cpnt {
 struct Particle {
     float lifetime;
     float maxLifetime;
-
-    SnapshotMeta snapshot_meta;
 };
 
 } // namespace cpnt
