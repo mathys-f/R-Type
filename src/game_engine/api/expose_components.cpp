05213cc5efa07aa8fdcdfc2ed409c1e0e4926b55
mathys-f <mathys.fillion@epitech.eu>
2026-01-05 10:47:03 +0100

    🐛 Fix Lua component exposure with improved property accessors for UITransform, UIStyle, and UINavigation

--- src/game_engine/api/expose_components.cpp
+++ src/game_engine/api/expose_components.cpp
@@ -17,23 +17,37 @@ void lua::expose_components(sol::state& lua) {
         "b", sol::property([](utils::Color& c) { return c.b; }, [](utils::Color& c, unsigned char v) { c.b = v; }),
         "a", sol::property([](utils::Color& c) { return c.a; }, [](utils::Color& c, unsigned char v) { c.a = v; }));
 
-    lua.new_usertype<cpnt::UITransform>("UITransform", "x", &cpnt::UITransform::x, "y", &cpnt::UITransform::y, "z",
-                                        &cpnt::UITransform::z, "w", &cpnt::UITransform::w, "h", &cpnt::UITransform::h,
-                                        "anchor_x", &cpnt::UITransform::anchor_x, "anchor_y",
-                                        &cpnt::UITransform::anchor_y, "rotation", &cpnt::UITransform::rotation);
+    lua.new_usertype<cpnt::UITransform>(
+        "UITransform",
+        "x", sol::property([](cpnt::UITransform& t) { return t.x; }, [](cpnt::UITransform& t, float v) { t.x = v; }),
+        "y", sol::property([](cpnt::UITransform& t) { return t.y; }, [](cpnt::UITransform& t, float v) { t.y = v; }),
+        "z", sol::property([](cpnt::UITransform& t) { return t.z; }, [](cpnt::UITransform& t, float v) { t.z = v; }),
+        "w", sol::property([](cpnt::UITransform& t) { return t.w; }, [](cpnt::UITransform& t, float v) { t.w = v; }),
+        "h", sol::property([](cpnt::UITransform& t) { return t.h; }, [](cpnt::UITransform& t, float v) { t.h = v; }),
+        "anchor_x", sol::property([](cpnt::UITransform& t) { return t.anchor_x; }, [](cpnt::UITransform& t, float v) { t.anchor_x = v; }),
+        "anchor_y", sol::property([](cpnt::UITransform& t) { return t.anchor_y; }, [](cpnt::UITransform& t, float v) { t.anchor_y = v; }),
+        "rotation", sol::property([](cpnt::UITransform& t) { return t.rotation; }, [](cpnt::UITransform& t, float v) { t.rotation = v; }));
 
     lua.new_usertype<cpnt::UIStyle>(
-        "UIStyle", "background_color", &cpnt::UIStyle::background_color, "background_color_hovered",
-        &cpnt::UIStyle::background_color_hovered, "background_color_pressed", &cpnt::UIStyle::background_color_pressed,
-        "text_color", &cpnt::UIStyle::text_color, "text_color_hovered", &cpnt::UIStyle::text_color_hovered,
-        "text_color_pressed", &cpnt::UIStyle::text_color_pressed, "border_color", &cpnt::UIStyle::border_color,
-        "border_color_hovered", &cpnt::UIStyle::border_color_hovered, "border_color_pressed",
-        &cpnt::UIStyle::border_color_pressed, "border_thickness", &cpnt::UIStyle::border_thickness, "border_radius",
-        &cpnt::UIStyle::border_radius, "border_thickness", &cpnt::UIStyle::border_thickness);
+        "UIStyle",
+        "background_color", sol::property([](cpnt::UIStyle& s) { return s.background_color; }, [](cpnt::UIStyle& s, utils::Color v) { s.background_color = v; }),
+        "background_color_hovered", sol::property([](cpnt::UIStyle& s) { return s.background_color_hovered; }, [](cpnt::UIStyle& s, utils::Color v) { s.background_color_hovered = v; }),
+        "background_color_pressed", sol::property([](cpnt::UIStyle& s) { return s.background_color_pressed; }, [](cpnt::UIStyle& s, utils::Color v) { s.background_color_pressed = v; }),
+        "text_color", sol::property([](cpnt::UIStyle& s) { return s.text_color; }, [](cpnt::UIStyle& s, utils::Color v) { s.text_color = v; }),
+        "text_color_hovered", sol::property([](cpnt::UIStyle& s) { return s.text_color_hovered; }, [](cpnt::UIStyle& s, utils::Color v) { s.text_color_hovered = v; }),
+        "text_color_pressed", sol::property([](cpnt::UIStyle& s) { return s.text_color_pressed; }, [](cpnt::UIStyle& s, utils::Color v) { s.text_color_pressed = v; }),
+        "border_color", sol::property([](cpnt::UIStyle& s) { return s.border_color; }, [](cpnt::UIStyle& s, utils::Color v) { s.border_color = v; }),
+        "border_color_hovered", sol::property([](cpnt::UIStyle& s) { return s.border_color_hovered; }, [](cpnt::UIStyle& s, utils::Color v) { s.border_color_hovered = v; }),
+        "border_color_pressed", sol::property([](cpnt::UIStyle& s) { return s.border_color_pressed; }, [](cpnt::UIStyle& s, utils::Color v) { s.border_color_pressed = v; }),
+        "border_thickness", sol::property([](cpnt::UIStyle& s) { return s.border_thickness; }, [](cpnt::UIStyle& s, float v) { s.border_thickness = v; }),
+        "border_radius", sol::property([](cpnt::UIStyle& s) { return s.border_radius; }, [](cpnt::UIStyle& s, float v) { s.border_radius = v; }));
 
-    lua.new_usertype<cpnt::UINavigation>("UINavigation", "up", &cpnt::UINavigation::up_tag, "down",
-                                         &cpnt::UINavigation::down_tag, "left", &cpnt::UINavigation::left_tag, "right",
-                                         &cpnt::UINavigation::right_tag);
+    lua.new_usertype<cpnt::UINavigation>(
+        "UINavigation",
+        "up", sol::property([](cpnt::UINavigation& n) { return n.up_tag; }, [](cpnt::UINavigation& n, ecs::TagRegistry::TagId v) { n.up_tag = v; }),
+        "down", sol::property([](cpnt::UINavigation& n) { return n.down_tag; }, [](cpnt::UINavigation& n, ecs::TagRegistry::TagId v) { n.down_tag = v; }),
+        "left", sol::property([](cpnt::UINavigation& n) { return n.left_tag; }, [](cpnt::UINavigation& n, ecs::TagRegistry::TagId v) { n.left_tag = v; }),
+        "right", sol::property([](cpnt::UINavigation& n) { return n.right_tag; }, [](cpnt::UINavigation& n, ecs::TagRegistry::TagId v) { n.right_tag = v; }));
 
     lua.new_usertype<cpnt::UIButton>("UIButton");
 }
