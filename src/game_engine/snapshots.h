d25c4cee9f02bd88ac280e5e1e8df9852e861e0a
JeanBizeul <jean.bizeul@epitech.eu>
2026-01-13 16:17:37 +0100

    ✨ Add structs to define the deltas sent via network

--- src/game_engine/snapshots.h
+++ src/game_engine/snapshots.h
@@ -34,23 +34,15 @@ struct SerializedComponent {
 
     std::byte *serialize() const;
     static SerializedComponent deserialize(std::byte const* data_ptr);
-    std::uint32_t get_serialized_size() const;
 };
 
 struct EntitySnapshot {
     std::uint32_t entity_id;
     std::vector<SerializedComponent> components;
-
-    std::byte *serialize() const;
-    static EntitySnapshot deserialize(std::byte const* data_ptr);
-    std::uint32_t get_serialized_size() const;
 };
 
 struct WorldSnapshot {
     std::vector<EntitySnapshot> entities;
-
-    std::byte *serialize() const;
-    static WorldSnapshot deserialize(std::byte const* data_ptr);
 };
 
 // This wrapper adds metadata to the WorldSnapshot
@@ -58,6 +50,27 @@ struct SnapshotRecord {
     WorldSnapshot snapshot;
     bool acknowledged = false;
     std::uint32_t last_update_tick;
+};
+
+enum class DeltaOperation : std::uint8_t {
+    entity_add,
+    entity_remove,
+    component_add_or_update,
+    component_remove
+};
+
+struct DeltaEntry {
+    DeltaOperation operation;
+    std::uint32_t entity_id;
+    SerializedComponent component; // Only used for component add or update
+};
+
+struct WorldDelta {
+    std::uint32_t base_snapshot_tick;
+    std::vector<DeltaEntry> entries;
+
+    std::byte* serialize() const;
+    static WorldDelta deserialize(const std::byte const* data_ptr);
 };
 
 } // namespace engn
