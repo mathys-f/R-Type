316e40b6e19a10c8d653f6e8a8dfa7a5499fa1c7
Léopold PINTARD <146716915+Leopold-PINTARD@users.noreply.github.com>
2026-01-13 11:49:04 +0100

    🤡 Remove a stress test

--- src/game_engine/engine.h
+++ src/game_engine/engine.h
@@ -54,7 +54,7 @@ class EngineContext {
     const size_t k_particles = 3;
     const size_t k_stars = 1000;
     const size_t k_max_bullets = 100;
-    const size_t k_max_enemies = 800000;
+    const size_t k_max_enemies = 8;
 
     ControlScheme controls = make_default_controls();
     ControlAction pending_rebind = ControlAction::None;
