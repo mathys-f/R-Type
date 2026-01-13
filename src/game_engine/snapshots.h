2abb2c18963b32171da470b7685797bc4eac025c
JeanBizeul <jean.bizeul@epitech.eu>
2026-01-13 17:35:02 +0100

    ✨ Add serialize & deserialize for the WorlDelta struct

--- src/game_engine/snapshots.h
+++ src/game_engine/snapshots.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <typeindex>
 #include <cstddef>
+#include <optional>
 
 namespace engn {
 
@@ -34,6 +35,7 @@ struct SerializedComponent {
 
     std::byte *serialize() const;
     static SerializedComponent deserialize(std::byte const* data_ptr);
+    std::uint32_t get_serialized_size() const;
 };
 
 struct EntitySnapshot {
@@ -62,7 +64,9 @@ enum class DeltaOperation : std::uint8_t {
 struct DeltaEntry {
     DeltaOperation operation;
     std::uint32_t entity_id;
-    SerializedComponent component; // Only used for component add or update
+
+    ComponentType component_type; // Only used for component remove
+    std::optional<SerializedComponent> component; // Only used for component add or update
 };
 
 struct WorldDelta {
@@ -70,7 +74,8 @@ struct WorldDelta {
     std::vector<DeltaEntry> entries;
 
     std::byte* serialize() const;
-    static WorldDelta deserialize(const std::byte const* data_ptr);
+    static WorldDelta deserialize(const std::byte *data_ptr);
+    std::uint32_t get_serialized_size() const;
 };
 
 } // namespace engn
