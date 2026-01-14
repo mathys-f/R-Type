1304caf6f9f5e6c84974268369c10fb696837cd1
nathan bezard <nathan.bezard@epitech.eu>
2026-01-14 15:01:46 +0100

    🩹 Remove the offset in shooter

--- src/game_engine/components/shooter.cpp
+++ src/game_engine/components/shooter.cpp
@@ -14,7 +14,6 @@ engn::SerializedComponent Shooter::serialize() const {
     // NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     std::size_t offset = 0;
     std::memcpy(serialized.data.data() + offset, &timer, sizeof(timer));
-    offset += sizeof(timer);
     // NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     return serialized;
 }
@@ -26,7 +25,6 @@ void Shooter::deserialize(const std::vector<std::byte>& data) {
     if (data.size() >= size) {
         std::size_t offset = 0;
         std::memcpy(&timer, data.data() + offset, sizeof(timer));
-        offset += sizeof(timer);
     }
     // NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 }
